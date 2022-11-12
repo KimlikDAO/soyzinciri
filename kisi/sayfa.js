e2a48593baf12d0d2c33278054bf561f1a346a60
d0x471b <0x471@protonmail.com>
2022-11-12 20:28:38 +0300

    Add images from array feature

--- kisi/sayfa.js
+++ kisi/sayfa.js
@@ -5,3 +5,18 @@ dom.adla("kib").onclick = () => {
     TCKT.yüzGöster(true);
 } 
 
+const grandpaImages = ["grandpa.png", "grandpa1.jpeg", "grandpa2.webp", "grandpa3.jpeg", "grandpa4.webp"];
+const galleryDiv = dom.adla("kigal");
+const avatarDiv = dom.adla("tca");
+
+
+for(let i = 1; i < grandpaImages.length; i++) {
+    let galleryElement = galleryDiv.children[0].cloneNode(true);
+    galleryElement.children[0].src = `/birim/tckt/img/${grandpaImages[i]}`;
+    galleryElement.style.display = "";  
+    galleryDiv.appendChild(galleryElement);
+}
+
+
+avatarDiv.children[0].src = `/birim/tckt/img/${grandpaImages[0]}`;
+avatarDiv.children[0].style.display = "";
