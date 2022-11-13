4aef1c9d4aadeb7eb7c1bcec02d20b77e90cab6b
Muhammet Coşkun <mcoskun86@gmail.com>
2022-11-13 12:15:07 +0300

    image ekle

--- soyzinciri/sayfa.js
+++ soyzinciri/sayfa.js
@@ -1,5 +1,6 @@
 import Cüzdan from '/birim/cüzdan/birim'
 import dom from '/lib/util/dom'
+// import {images} from './data/'
 
 dom.adla("na").onclick = Cüzdan.bağla();
 dom.adla("socnc").onclick = Cüzdan.bağla();
@@ -7,8 +8,8 @@ dom.adla("socnc").onclick = Cüzdan.bağla();
 const soy = {
   "0x471": {
     "personInfo": {
-      first: "Deniz",
-      last: "SoyBoy",
+      first: "Kaan",
+      last: "Ankara",
     },
     "soyZinciri": {
       anneEvm: "0x111anne",
@@ -17,55 +18,55 @@ const soy = {
     }
   }, "0x111anne": {
     "personInfo": {
-      first: "",
-      last: "",
+      first: "Fatma",
+      last: "Ankara",
     },
     "soyZinciri": {
       anneEvm: "0x333anneanne",
       babaEvm: "0x444annebaba",
-      profilResmi: "",
+      profilResmi: "https://pickaface.net/gallery/avatar/unr_karen_171017_1443_9ujb42.png",
     }
   }, "0x222baba": {
     "personInfo": {
-      first: "",
-      last: "",
+      first: "Mehmet",
+      last: "Ankara",
     },
     "soyZinciri": {
-      anneEvm: "0x555babaanne",
-      babaEvm: "0x666babababa",
-      profilResmi: "",
+      anneEvm: "0xB75511a03D747e128dEEFB97832D4604350Bf18e",
+      babaEvm: "0x1C66D49FB1e9782Aa838A2Ec9fa6F346C85096E0",
+      profilResmi: "https://tr.rbxcdn.com/24164d57321dda1de4a727f1af07a965/420/420/Image/Png",
     }
   }, "0x333anneanne": {
     "personInfo": {
-      first: "",
-      last: "",
+      first: "Elif",
+      last: "Demir",
     },
     "soyZinciri": {
-      profilResmi: "",
+      profilResmi: "https://cdn4.iconfinder.com/data/icons/cool-avatars-2/190/00-36-512.png",
     }
   }, "0x444annebaba": {
     "personInfo": {
-      first: "",
-      last: "",
+      first: "Murat",
+      last: "Demir",
     },
     "soyZinciri": {
-      profilResmi: "",
+      profilResmi: "https://www.clipartmax.com/png/middle/337-3375762_old-man-with-glasses-icon-clipart-computer-icons-avatar-old-man-icons.png",
     }
-  }, "0x555babaanne": {
+  }, "0xB75511a03D747e128dEEFB97832D4604350Bf18e": {
     "personInfo": {
-      first: "",
-      last: "",
+      first: "Hayriye",
+      last: "Ankara",
     },
     "soyZinciri": {
-      profilResmi: "",
+      profilResmi: "https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/grandma_elderly_nanny_avatar-512.png",
     }
-  }, "0x666babababa": {
+  }, "0x1C66D49FB1e9782Aa838A2Ec9fa6F346C85096E0": {
     "personInfo": {
-      first: "",
-      last: "",
+      first: "Hayri",
+      last: "Ankara",
     },
     "soyZinciri": {
-      profilResmi: "",
+      profilResmi: "https://media.istockphoto.com/vectors/vector-portrait-of-old-man-vector-id498421354?s=612x612",
     }
   }
 }
