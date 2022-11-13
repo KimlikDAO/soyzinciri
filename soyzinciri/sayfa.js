006796a17f3b6113e6ed41d46755cba0ac00b6c1
Muhammet Coşkun <mcoskun86@gmail.com>
2022-11-13 11:45:09 +0300

    soy sayfasi htmle dagit

--- soyzinciri/sayfa.js
+++ soyzinciri/sayfa.js
@@ -4,30 +4,96 @@ import dom from '/lib/util/dom'
 dom.adla("na").onclick = Cüzdan.bağla();
 dom.adla("socnc").onclick = Cüzdan.bağla();
 
-const aile = [{
+const soy = {
   "0x471": {
-    anneEvm: "0x111anne",
-    babaEvm: "0x222baba",
+    "personInfo": {
+      first: "Deniz",
+      last: "SoyBoy",
+    },
+    "soyZinciri": {
+      anneEvm: "0x111anne",
+      babaEvm: "0x222baba",
+      profilResmi: "https://www.w3schools.com/howto/img_avatar.png",
+    }
+  }, "0x111anne": {
+    "personInfo": {
+      first: "",
+      last: "",
+    },
+    "soyZinciri": {
+      anneEvm: "0x333anneanne",
+      babaEvm: "0x444annebaba",
+      profilResmi: "",
+    }
+  }, "0x222baba": {
+    "personInfo": {
+      first: "",
+      last: "",
+    },
+    "soyZinciri": {
+      anneEvm: "0x555babaanne",
+      babaEvm: "0x666babababa",
+      profilResmi: "",
+    }
+  }, "0x333anneanne": {
+    "personInfo": {
+      first: "",
+      last: "",
+    },
+    "soyZinciri": {
+      profilResmi: "",
+    }
+  }, "0x444annebaba": {
+    "personInfo": {
+      first: "",
+      last: "",
+    },
+    "soyZinciri": {
+      profilResmi: "",
+    }
+  }, "0x555babaanne": {
+    "personInfo": {
+      first: "",
+      last: "",
+    },
+    "soyZinciri": {
+      profilResmi: "",
+    }
+  }, "0x666babababa": {
+    "personInfo": {
+      first: "",
+      last: "",
+    },
+    "soyZinciri": {
+      profilResmi: "",
+    }
   }
-}, {
-  "0x111anne": {
-    anneEvm: "0x333anneanne",
-    babaEvm: "0x444annebaba",
-  }
-}, {
-  "0x222baba": {
-    anneEvm: "0x555babaanne",
-    babaEvm: " 0x666babababa"
-  }
-}]
+}
 
+const htmleDagıt = (adres, id) => {
+  const element = dom.adla(id);
+  element.children[0].children[0].src = soy[adres]["soyZinciri"]["profilResmi"];
+  element.children[1].innerText = soy[adres]["personInfo"]["first"] + " " + soy[adres]["personInfo"]["last"];
+  element.onclick = () => window.location.href = `kisi/${adres}`;
+}
+console.log(soy)
 Cüzdan.bağlanınca(() => {
   dom.adlaGizle("socnc");
   dom.adlaGöster("socb");
-  dom.adla("sok").onclick = () => window.location.href = `/kisi/${Cüzdan.adres()}`;
-  dom.adla("soa").onclick = () => {
-    window.location.href = `/kisi/${aile[0]["0x471"]["anneEvm"]}`;
-    console.log(window.location.href)
-    console.log(aile)
-  }
+  const anneEvmAdres = soy["0x471"]["soyZinciri"]["anneEvm"];
+  const babaEvmAdres = soy["0x471"]["soyZinciri"]["babaEvm"];
+  const anneAnneEvmAdres = soy[anneEvmAdres]["soyZinciri"]["anneEvm"];
+  const anneBabaEvmAdres = soy[anneEvmAdres]["soyZinciri"]["babaEvm"];
+  const babaAnneEvmAdres = soy[babaEvmAdres]["soyZinciri"]["anneEvm"];
+  const babaBabaEvmAdres = soy[babaEvmAdres]["soyZinciri"]["babaEvm"];
+  const map = [
+    ["0x471", "sok"],
+    [anneEvmAdres, "soa"],
+    [babaEvmAdres, "sob"],
+    [anneAnneEvmAdres, "soaa"],
+    [anneBabaEvmAdres, "soab"],
+    [babaAnneEvmAdres, "soba"],
+    [babaBabaEvmAdres, "sobb"]
+  ]
+  map.map(((element) => htmleDagıt(element[0], element[1])));
 })
